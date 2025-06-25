72fb76e6bf9dae2f3944debd5907a5ef476c8130
Indupriya Bejugam <indupriya.bejugam@gmail.com>
2025-06-26 01:09:04 +0530

    Update vite.config.js for accessing from ec2

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,5 +3,9 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  server: {
+    host: true,       // <--- add this line
+    port: 5173,       // optional, just to be explicit
+  },
   plugins: [react()],
-})
+});
